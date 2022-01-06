732883cac29b44a246f0e0d930e9982f10197b67
Xuzhao Wang <56864878+WXZhao7@users.noreply.github.com>
2022-01-06 19:46:40 +0800

    update js

--- VSCode4GitHub.user.js
+++ VSCode4GitHub.user.js
@@ -11,14 +11,14 @@
 
 (function() {
     'use strict';
-    var VSCodeWeb = document.createElement("loc");
+    var VSCodeWeb = document.createElement("li");
     VSCodeWeb.innerHTML ='Code';
     VSCodeWeb.className='open-on-sourcegraph btn btn-sm tooltipped tooltipped-s';
-    var loc=document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).getElementsByTagName('loc')[0]
-    document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).insertBefore(VSCodeWeb,loc)
+    var li=document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).getElementsByTagName('li')[0]
+    document.getElementsByClassName('pagehead-actions flex-shrink-0 d-none d-md-inline').item(0).insertBefore(VSCodeWeb,li)
     VSCodeWeb.onclick=function()
     {
-        var href=top.location.href.replace('github.com','vscode.dev/github')
-        top.location.href=href;
+        var href=top.liation.href.replace('github.com','vscode.dev/github')
+        top.liation.href=href;
     }
 })();
